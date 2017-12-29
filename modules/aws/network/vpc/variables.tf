f427ee92b9831f8f9f0b853a67ec4c27e9645798
Artur Kiryiak <ogurec001@gmail.com>
2017-12-29 17:15:33 +0300

    Update variables.tf

--- modules/aws/network/vpc/variables.tf
+++ modules/aws/network/vpc/variables.tf
@@ -1,15 +1,15 @@
 variable "cidr_block" {
   default = "172.31.0.0/16"
 }
-variable "vpc_enable_dns_hostnames" {
+variable "enable_dns_hostnames" {
   default = "true"
 }
-variable "vpc_enable_dns_support" {
+variable "enable_dns_support" {
   default = "true"
 }
-variable "vpc_name" {
+variable "name" {
   default = "main"
 }
-variable "vpc_environment" {
+variable "environment" {
   default = "production"
 }
