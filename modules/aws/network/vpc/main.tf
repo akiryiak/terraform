fc3f7d392f3e35ff2fedc1771fd34f6e39a6c6d2
Artur Kiryiak <ogurec001@gmail.com>
2017-12-29 17:10:36 +0300

    Update main.tf
    variables names fix

--- modules/aws/network/vpc/main.tf
+++ modules/aws/network/vpc/main.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "vpc" {
   cidr_block            = "${var.cidr_block}"
-  enable_dns_hostnames  = "${var.vpc_enable_dns_hostnames}"
-  enable_dns_support    = "${var.vpc_enable_dns_support}"
+  enable_dns_hostnames  = "${var.enable_dns_hostnames}"
+  enable_dns_support    = "${var.enable_dns_support}"
 
   tags = {
-    Name                = "${var.vpc_name}"
+    Name                = "${var.name}"
     terraform           = "true"
-    environment         = "${var.vpc_environment}"
+    environment         = "${var.environment}"
   }
 }
